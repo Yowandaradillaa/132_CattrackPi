76d66b91ea3aa36870d272c60ccf5ed3a8ef5ea1
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-01-04 21:15:36 +0700

    Memperbaiki error

--- cattrack-frontend/vite.config.js
+++ cattrack-frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001, // Tulis 3001 di sini
+    strictPort: true, // Opsional: Agar Vite tidak otomatis pindah ke port lain kalau 3001 dipakai
+  }
 })
