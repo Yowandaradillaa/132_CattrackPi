c9eab47082ed017381bf7dc03b7b76068b23c2c7
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-01-04 18:37:26 +0700

    menambahkan registrasi dengan apikey

---
+++ cattrack-backend/controllers/authController.js
@@ -0,0 +1,25 @@
+const db = require('../config/db');
+const bcrypt = require('bcryptjs');
+const { v4: uuidv4 } = require('uuid');
+
+exports.register = async (req, res) => {
+    const { nama, email, password } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const apiKey = uuidv4();
+        
+        // Secara default role adalah 'user'
+        await db.query('INSERT INTO users (nama, email, password, api_key, role) VALUES (?, ?, ?, ?, "user")', 
+            [nama, email, hashedPassword, apiKey]);
+        
+        res.status(201).json({ 
+            message: 'Registrasi Developer Berhasil', 
+            api_key: apiKey, 
+            nama: nama,
+            role: 'user'
+        });
+    } catch (err) { 
+        res.status(500).json({ error: err.message }); 
+    }
+};
+
