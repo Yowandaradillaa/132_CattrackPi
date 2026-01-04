45b13a5a7173fa675b1c574d4eee30420b9526f3
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-01-04 18:51:24 +0700

    menambahkan CRUD data kucing

--- cattrack-backend/controllers/kucingController.js
+++ cattrack-backend/controllers/kucingController.js
@@ -19,3 +19,20 @@ exports.getById = async (req, res) => {
     }
 };
 
+// Hanya Admin yang bisa menambah, mengubah, dan menghapus
+exports.create = async (req, res) => {
+    const { nama, jenis, umur } = req.body;
+    await db.query('INSERT INTO kucing (user_id, nama, jenis, umur) VALUES (?, ?, ?, ?)', [req.user.id, nama, jenis, umur]);
+    res.json({ message: 'Data Kucing Publik Berhasil Ditambahkan (Admin Only)' });
+};
+
+exports.update = async (req, res) => {
+    const { nama, jenis, umur } = req.body;
+    await db.query('UPDATE kucing SET nama=?, jenis=?, umur=? WHERE id=?', [nama, jenis, umur, req.params.id]);
+    res.json({ message: 'Data Kucing Publik Berhasil Diupdate' });
+};
+
+exports.delete = async (req, res) => {
+    await db.query('DELETE FROM kucing WHERE id=?', [req.params.id]);
+    res.json({ message: 'Data Kucing Publik Berhasil Dihapus' });
+};
