3f6ef13c0f3ac35fc9ac224987df8e343b80375a
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-01-04 19:05:07 +0700

    menambahkan middleware autentikasi apikey

---
+++ cattrack-backend/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const db = require('../config/db');
+
+// Middleware untuk cek apakah API Key Valid
+const authenticateApiKey = async (req, res, next) => {
+    const apiKey = req.header('x-api-key');
+    if (!apiKey) return res.status(401).json({ message: 'API Key diperlukan (x-api-key)' });
+
+    try {
+        const [rows] = await db.query('SELECT * FROM users WHERE api_key = ?', [apiKey]);
+        if (rows.length === 0) return res.status(403).json({ message: 'API Key tidak valid' });
+        
+        req.user = rows[0]; // Menyimpan data user (termasuk role) ke request
+        next();
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
